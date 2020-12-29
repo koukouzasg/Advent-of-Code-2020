7f647949031312c99e28b9790754201386c64ccc
koukouzasg <koukouzasg@hotmail.gr>
2020-12-29 21:24:24 +0200

    Add Day6

--- Day Six/Day6_P2.py
+++ Day Six/Day6_P2.py
@@ -1,17 +1,23 @@
+import collections
+
 with open("C:\Programming\github\Advent-of-Code-2020\Day Six\Day6 Input.txt") as f:
     input = [line.strip() for line in f]
 
 input.append("")    
-answers, totalSum = [], 0
+answers, totalSum, linesCounter = [], 0, 0
 
-for i, answer in enumerate(input):
+for answer in input:
     if answer == "":
-        #totalSum += len(answers)
-        print(answers)
-        break
+        frequencies = collections.Counter(answers)
+        repeated = {}
+        for key, value in frequencies.items():
+            if value == linesCounter:
+                totalSum += 1
         answers = []
+        linesCounter = 0
     else:
-        answers.append(answer)
-  
+        linesCounter += 1
+        for c in answer:
+            answers.append(c)
 
 print(totalSum)
